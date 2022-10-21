3571d639bcd8018ebc6fa080b0b8d7c7f17a13b2
Diogo Valente Polonia <diogovalentepcs>
2022-10-21 01:49:57 -0300

    Fix requirements

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import linkedin_api
 import streamlit as st
 import pandas as pd
 import pandas as pd
-from traitlets import default
+#from traitlets import default
 import tweepy
 import numpy as np
 import matplotlib.pyplot as plt
