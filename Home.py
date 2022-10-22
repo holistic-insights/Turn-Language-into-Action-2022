aa8c731893e308720dea190b6b498d2279477d92
Diogo Valente Polonia <diogovalentepcs>
2022-10-22 06:45:42 -0300

    Add new pages

--- Home.py
+++ Home.py
@@ -1,31 +1,54 @@
 from operator import ge, index
 from os import link
-import linkedin_api
 import streamlit as st
 import pandas as pd
 import pandas as pd
-#from traitlets import default
-import tweepy
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 # import API scripts
 import linkedinAPI 
 import expertaiAPI
+from streamlit.components.v1 import html
+
+@st.cache
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
 
 st.title("ESG Glassdoor")
 
 st.write("Raising transparency on companies' attitude towards ESG and compare their position with public perception.")
 
+
 col1, col2, col3 = st.columns(3)
 with col1:
-    #company_name = st.text_input("Company name", value="Walmart")
     company_name = st.selectbox("Company", (list(linkedinAPI.getPosts().company.unique())), index=3)
-
 with col2:
     since_date = st.text_input("Get posts since", value="YYYY-MM-DD")
 with col3:
-    linkedin_count = st.slider("Number of LinkedIn posts", min_value=100, max_value=1000, step=10, value=0) 
+    linkedin_count = st.slider("Max LinkedIn posts", min_value=100, max_value=1000, step=10, value=0) 
 
 keywords = st.multiselect(
     'Hashtags',
@@ -33,110 +56,15 @@ keywords = st.multiselect(
 
 since_date = since_date[:7] + '--' + since_date[8:]
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    show_posts = st.checkbox('Show Post Data')
+col1, col2, col3, col4 = st.columns(4)
 with col2:
-    show_analysis = st.checkbox('Show Analysis Data')
+    company_analysis = st.button('Analyse this Company')
 with col3:
-    submit = st.button('Submit')
-
-if submit:
-   
-    # Scrape LinkedIn
-    raw_posts = linkedinAPI.getPosts()
-    raw_comments = linkedinAPI.getComments()
-
-    # Do ESG & Sentiment Analysis
-    posts, comments = expertaiAPI.magic(raw_posts, raw_comments)
-    company_posts = posts[posts.company == company_name.lower()]
-    company_comments = comments[comments.company == company_name.lower()]
-
-    st.write("---")
-    
-    # Summary Estatistics
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown(f"<h4 style='text-align: center;'>{company_name}</h4>", unsafe_allow_html=True)
-    with col2:
-        st.markdown(f"<h4 style='text-align: center;'>LinkedIn Top 10</h4>", unsafe_allow_html=True)
-    # Scores
-    col3, col4, col5, col6 = st.columns(4)
-    style = "style='text-align: center; background: #F0F8FF; padding-top: 25px; padding-bottom: 25px;'"
-    with col3:
-        st.markdown(f"<h3 {style}>{round(company_posts.ESG.mean(), 2)}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>ESG Score</p>", unsafe_allow_html=True)
-    with col4:
-        st.markdown(f"<h3 {style}>{round(company_posts.Sentiment.mean(), 2)}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>Sentiment Score</p>", unsafe_allow_html=True)
-    with col5:
-        st.markdown(f"<h3 {style}>{round(posts.ESG.mean(), 2)}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>ESG Score</p>", unsafe_allow_html=True)
-    with col6:
-        st.markdown(f"<h3 {style}>{round(posts.Sentiment.mean(), 2)}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>Sentiment Score</p>", unsafe_allow_html=True)
-    # posts and comments
-    col3, col4, col5, col6 = st.columns(4)
-    style = "style='text-align: center; background: #F0F8FF; padding-top: 25px; padding-bottom: 25px;'"
-    with col3:
-        st.markdown(f"<h3 {style}>{len(company_posts.post_urn)}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>Posts</p>", unsafe_allow_html=True)
-    with col4:
-        st.markdown(f"<h3 {style}>{round(company_posts.numComments.mean())}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>Comments (Avg)</p>", unsafe_allow_html=True)
-    with col5:
-        st.markdown(f"<h3 {style}>{len(posts.post_urn)}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>Posts</p>", unsafe_allow_html=True)
-    with col6:
-        st.markdown(f"<h3 {style}>{round(posts.numComments.mean())}</h3", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center;'>Comments (Avg)</p>", unsafe_allow_html=True)
-
-    st.write("---")
-
-    # Show Raw Posts
-    if show_posts:
-        st.write("Raw Posts Data")
-        st.write(raw_posts)
-
-    # Show Analysed Posts  
-    if show_analysis:
-        st.write("Analysed Posts Data")
-        st.write(company_posts)
-
-    # Visualizations
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown(f"<h4 style='text-align: center;'>ESG x Sentiment</h4>", unsafe_allow_html=True)
-        fig, ax = plt.subplots()
-        ax.scatter(x=company_posts["Sentiment"], y=company_posts["ESG"], cmap='veridis')
-        try:
-            z = np.polyfit(company_posts["Sentiment"], company_posts["ESG"], 1)
-            p = np.poly1d(z)
-            ax.plot(company_posts["Sentiment"], p(company_posts["Sentiment"]),"r--")
-        except:
-            pass
-        sns.despine(left = True)
-        plt.ylabel('ESG Score')
-        plt.xlabel('Sentiment Score')
-        st.pyplot(fig)
+    post_analysis = st.button('Try out your own Posts')
 
 
-
-    with col2:
-        st.markdown(f"<h4 style='text-align: center;'>ESG Scores</h4>", unsafe_allow_html=True)
-        cols = ['Biodiversity', 'Gender_Equality', 'Strategy']
-        means = []
-        for col in cols:
-            means.append(company_posts[col].mean())
-
-        df = pd.DataFrame({'Score' : means,
-                            'Type' : ['Environmental', 'Social', 'Governance']},
-                            index = cols)
-        fig, ax = plt.subplots()
-
-        colors = tuple(np.select([df["Type"] == 'Environmental',df["Type"] == 'Social',df["Type"] == 'Governance'], ['palegreen', 'plum', 'gainsboro']))
-
-        ax.barh(width=df.Score, y=df.index, align='center', color = colors)
-        sns.despine(left = True)
-
-        st.pyplot(fig)
+if company_analysis:
+    nav_page("Company_Analysis")
+if post_analysis:
+    nav_page("Post_Analysis")
+ 
