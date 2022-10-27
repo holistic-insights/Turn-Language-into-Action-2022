1bf0bf0505c49e4ae829ea74505c973d4566fe43
João Afonso Pereira <joaoafonsoppereira@gmail.com>
2022-10-27 19:38:49 +0100

    try colors

--- Home.py
+++ Home.py
@@ -389,7 +389,7 @@ with tab1:
                 fig = dict(data=[fig], layout=layout)
                 st.plotly_chart(fig, use_container_width=True)
 
-            cat_counts_df['Category'] = cat_counts_df['Category'].apply(lambda x: f'{x} ({option_name})')
+            cat_counts_df['Category'] = cat_counts_df['Category'].apply(lambda x: f'{x} (Comp. {option_name})')
             cat_counts_df_top5['Category'] = cat_counts_df_top5['Category'].apply(lambda x: f'{x} (Top 5)')
 
             cat_counts_df['Source'] = option_name
@@ -402,7 +402,7 @@ with tab1:
             st.markdown(f'<h4>ESG analysis</h4>', unsafe_allow_html=True)
 
             st.markdown(f'<h5>ESG categories counts</h5>', unsafe_allow_html=True)
-            fig = px.bar(data_frame=cat_counts_df_all.sort_values(by='Category'), x='Category', y='Counts', color='Category', color_discrete_sequence=['#B6E886', '#B6E886', '#FF6692', '#FF6692', '#19D3F3', '#19D3F3'])
+            fig = px.bar(data_frame=cat_counts_df_all.sort_values(by='Category'), x='Category', y='Counts', color='Category', color_discrete_sequence=['#B6E886', '#e2f6cf', '#FF6692', '#ffc2d3', '#19D3F3', '#a3edfa'])
             st.plotly_chart(fig, use_container_width=True)
 
             st.markdown(f'<h5>ESG categories scores</h5>', unsafe_allow_html=True)
