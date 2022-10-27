fix average counts ESG

Home.py
@@ -392,11 +392,11 @@ with tab1:
             cat_counts_df['Source'] = option_name
             cat_counts_df_top5['Source'] = 'Top 5'
 
-            cat_counts_df_top5['Counts'] = cat_counts_df_top5['Counts'].apply(lambda x: np.round(x/5))
+            cat_counts_df_top5['Counts'] = cat_counts_df_top5['Counts'].apply(lambda x: x/5)
 
             cat_counts_df_all = pd.concat([cat_counts_df, cat_counts_df_top5], ignore_index=True)
 
-            st.write(cat_counts_df_top5)
+            st.write(cat_counts_df_all)
 
             st.markdown(f'<h4>ESG analysis</h4>', unsafe_allow_html=True)
 